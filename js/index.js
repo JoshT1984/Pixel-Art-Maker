a0ca4bf92fc145c296bc8808354f7fbeff9400e9
JoshT1984 <warlord84502@yahoo.com>
2023-11-17 17:57:47 -0600

    Progress with event listener

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
 let insideContainer = document.querySelector("#inside-container");
 let canvas = document.createElement("div");
-
-insideContainer.append(canvas);
 canvas.className = "canvas";
+insideContainer.append(canvas);
 let canvasWidth = canvas.offsetWidth * 1.81;
 let canvasHeight = canvas.offsetHeight * 1.81;
+let pixelGrid;
+render();
 
+function render() {
+  makeGridLines();
+}
 function makeGridLines() {
-  
-  for (let i = 0; i < canvasWidth + 12; i++) {
-    let pixelGrid = document.createElement("div");
+  for (let i = 0; i < canvasWidth - 131; i++) {
+    pixelGrid = document.createElement("div");
     pixelGrid.className = "pixels";
 
     for (let j = 0; j < canvasHeight; j++) {
@@ -18,4 +21,9 @@ function makeGridLines() {
   }
 }
 
-makeGridLines();
+// let drawPixels = document.querySelectorAll(".pixels");
+// let drawPixels = document.querySelectorAll(".pixels");
+
+drawPixels.addEventListener("click", (e) => {
+  pixelGrid.style.backgroundColor = "black";
+});
